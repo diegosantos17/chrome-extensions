5408c794c4fdc2d384b8b8aeac7cb7fd95abe63b
diegosrtrainninggit <diegosr.trainning.git@gmail.com>
2020-08-03 15:15:29 -0300

    API para inclusão de campos

--- buonny-ext-estrang/inside.js
+++ buonny-ext-estrang/inside.js
@@ -1,32 +1,53 @@
 var tip, text;
 var fieldsSearch = [];
 var fieldsSearchSets = false;
+var fieldCount = 1;
+var urlApi = "https://buonny-mock-api-v3.herokuapp.com";
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {    
     console.log('Atualizado status estrangulamento...');
-    var divTopo = document.getElementsByClassName("enviroment-color");    
-
-    showLoader();    
+    var divTopo = document.getElementsByClassName("enviroment-color");
+    fieldCount = 1;
+    console.log('control-group', $('.control-group').length);    
+    $('.btnDetailP').remove();
+    console.log('cadilam-ext', $('.btnDetailP').length);    
+    showLoader(true);        
+    insertFormPage();
+    insertBtnDetailsFromPage();
+    setFormTooltipAction();
 
     if(divTopo != undefined && divTopo.length > 0){                        
         divTopo[0].style.borderColor = 'orange'
         divTopo[0].style.borderWidth = '3px'
         
         // Obtendo elementos da tela
-        var url  = "https://buonny-mock-api-v3.herokuapp.com/telas?url=" + getPageName();
+        var url  = urlApi + "/telas?url=" + getPageName();
         var xhr  = new XMLHttpRequest()
         xhr.open('GET', url, true)
-        xhr.onload = function () {
-
-            console.log('xhr', xhr)
-
+        xhr.onload = function () {            
             if(xhr.status == 200){
                 var telas = JSON.parse(xhr.responseText);                
 
                 if (xhr.readyState == 4 && xhr.status == "200") {                        
                     telas.fields.forEach(formatField);
-                    setStyleTooltip();    
-                    setBtnDetail();    
+                    //setStyleTooltip();
+                    //setBtnDetail();                                            
+                    setBtnDetailClick(telas._id);
+
+                    tableResultado = document.querySelectorAll('table[' + telas.tables[0].selector + ']');
+                    
+                    if(tableResultado != null && tableResultado.length > 0){
+                        console.log('tableResultado', tableResultado.offsetLeft, tableResultado.offsetTop);
+
+                        let thead = document.createElement('thead');
+                        let th = document.createElement('th');
+                        th.setAttribute('class', 'teste');
+                        th.innerText = fieldCount++;
+                        thead.appendChild(th);
+
+                        // insert a new node before the first list item
+                        tableResultado[0].insertBefore(thead, tableResultado[0].firstElementChild);
+                    }
                     document.getElementById('divLoader').style.display = 'none';
                     console.log('Status estrangulamento atualizado!');
                 } else {
@@ -38,19 +59,126 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
             }
         }
         xhr.send(null);
-    }    
+        }    
 })
+function setFormTooltipAction(){
+    $('#formTooltipField').submit(function(event){        
+
+        event.preventDefault();
+        var $form = $(this);
+        // Let's select and cache all the fields        
+        var $inputs = $form.find("input, select, button, textarea");
+
+        // Serialize the data in the form
+        var serializedData = $form.serialize();
+
+        console.log('serializedData', serializedData);
+
+        $.ajax({
+            url: urlApi + "/fields",
+            type: "post",
+            data: serializedData,
+            success: function(data, textStatus, jqXHR)
+            {
+                console(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+         
+            }
+        });
+
+        // $('#redmine_issue_id').val();
+        // $('#swagger_prop_req_name').val();
+        // $('#swagger_prop_resp_name').val();
+        // $('#input-link_resp_swagger').val();
+
+
+    })
+}
+
+function insertBtnDetailsFromPage(){
+
+    let $field;
+    let template = `
+        <div class="btnDetailP" 
+            data-id="{id}" 
+            data-name="{name}" 
+            data-class="{class}" 
+            data-title="{title}" 
+            data-label="{label}" 
+            data-tooltip-id="{idTooltip}">{value}
+        </div>
+    `
+    $('.control-group').each(function(item){
+        $field = $(this).children('input:not([type=hidden])').first();
+        console.log('$field', $field);
+
+        if($field.length == 0){                    
+            $field = $(this).children('select').first();
+        }
+        
+
+        if($field.length > 0){
+            let labelDescription = '';
+            let $labelField = $(this).children('label').first();
+
+            if($labelField != null && $labelField.length > 0)
+            {
+                labelDescription = $labelField.text();
+            }
+
+            let buttonDetail =  template
+            .replace('{id}', $field.attr("id"))
+            .replace('{name}', $field.attr("name"))
+            .replace('{class}', $field.attr("class"))
+            .replace('{title}', $field.attr("title"))
+            .replace('{label}', labelDescription)            
+            .replace('{idTooltip}', 'tol-' + $field.attr("id"))
+            .replace('{value}', fieldCount);
+
+            $(this).append(buttonDetail);
+            fieldCount++;
+        }
+    });
+
+    $('.row-fluid').each(function(item){
+        $field = $(this).children('input[type=submit]').first();
+        
+        if($field.length > 0){
+            let buttonDetail =  template
+            .replace('{id}', $field.attr("id"))
+            .replace('{name}', $field.attr("name"))
+            .replace('{class}', $field.attr("class"))
+            .replace('{title}', $field.attr("title"))
+            .replace('{idTooltip}', 'tol-' + $field.attr("id"))
+            .replace('{value}', fieldCount);
+
+            $(this).append(buttonDetail);
+            fieldCount++;
+        }
+    });
+    
+}
 
 function formatField(element, index, array) {    
     var field;
 
+    if(element.htmlAttribute != null && element.htmlAttribute != undefined){        
+        let fieldTemp = document.querySelectorAll(element.htmlDomTagName + "[" + element.htmlAttribute + "]");
+        if(fieldTemp != null && fieldTemp.length > 0){            
+            field = fieldTemp[0];            
+            field.setAttribute('custom-tag', element.htmlDomTagName + element.htmlAttributeValue);
+            field.setAttribute("id", element.htmlDomTagName + element.htmlAttributeValue);
+        }
+    }
     if(element.id == "BtnBuscar"){
         let fieldTemp = document.querySelectorAll('[value="Buscar"]');
         if(fieldTemp != null && fieldTemp.length > 0){
             field = fieldTemp[0];            
         }
 
-    } else {
+    } else if(element.id != ''){
         field = document.getElementById(element.id);
     }
     
@@ -64,8 +192,6 @@ function formatField(element, index, array) {
             field.setAttribute('tooltip', '')
         }
 
-
-
         if(element.request != null){
             field.setAttribute('request_prop', element.request.property);
         }
@@ -77,41 +203,80 @@ function setClass(field, status){
 
     switch(status){
         case 'mock':            
-            field.style.borderWidth = '2px';
+            field.style.borderWidth = '1px';
             field.style.borderColor = '#6A5ACD';
             field.style.borderStyle = 'dotted';
+            field.style.backgroundColor = '#d9babf';
             break;
         case 'hml':
-                field.style.borderWidth = '2px';
+                field.style.borderWidth = '1px';
                 field.style.borderColor = '#0000FF';
                 field.style.borderStyle = 'solid';
+                field.style.backgroundColor = 'lightblue';                
                 break;
         case 'prod':
-            field.style.borderWidth = '2px';
+            field.style.borderWidth = '1px';
             field.style.borderColor = 'green';
             field.style.borderStyle = 'solid';
+            field.style.backgroundColor = '#a6cda2';            
             break;
+        case 'devdone':
+            field.style.borderWidth = '1px';
+            field.style.borderColor = 'orange';
+            field.style.borderStyle = 'solid';
+            field.style.backgroundColor = 'lightyellow';            
+            break;                
         default:            
-            field.style.borderWidth = '2px';
+            field.style.borderWidth = '1px';
             field.style.borderColor = 'purple';            
             field.style.borderStyle = 'dotted';
             break;
     }
 }
 
+function setBtnDetailClick(idTela){
+    $('.btnDetailP').click(function(){
+        
+        $divFormPopUp2 = $('#divFormPopUp2');
+
+        if($divFormPopUp2.hasClass('show-tooltip')){ // oculta
+            $(this).removeClass('red');
+            $(this).addClass('green');
+
+            $divFormPopUp2.addClass('hidden-tooltip');
+            $divFormPopUp2.removeClass('show-tooltip');
+        } else { // exibe
+            $(this).addClass('red');
+            $(this).removeClass ('green');
+
+            $divFormPopUp2.removeClass('hidden-tooltip')
+            $divFormPopUp2.addClass('show-tooltip')            
+            //$divFormPopUp2.css({top: $(this).offset().top, left: $(this).offset().left + 30});                
+
+            $formTooltipField = $divFormPopUp2.children('form#formTooltipField');
+                        
+            $formTooltipField.children('input#idTela').val(idTela);
+            $formTooltipField.children('input#dom_id').val($(this).attr('data-id'));
+            $formTooltipField.children('input#dom_name').val($(this).attr('data-name'));
+            $formTooltipField.children('input#dom_title').val($(this).attr('data-title'));
+            $formTooltipField.children('input#dom_class').val($(this).attr('data-class'));            
+            $formTooltipField.children('input#dom_label').val($(this).attr('data-label'));            
+        }        
+    });
+}
 function setBtnDetail(){
 
-    var a = document.getElementsByTagName('*')
-    var fieldCount = 1;
+    var a = document.getElementsByTagName('*')    
     var request_prop = '';
 
     for (var x=0;x < a.length;x++) { // Cria o tooltip        
         text = a[x].getAttribute('tooltip');
-        
+                
         if (text != null) {// Verifica se existe texto no tooltip
             var btnDetail = document.createElement('div');
             btnDetail.innerHTML = fieldCount;   
-            
+            btnDetail.setAttribute("cadilam-ext", "true");
+
             if(a[x].getAttribute('id') == 'BtnBuscar'){
                 a[x].setAttribute('tooltip', setInfoFieldsSearch(a[x]))
                 setStyleTooltip(a[x]);
@@ -120,21 +285,15 @@ function setBtnDetail(){
 
             if(fieldsSearchSets == false){
                 request_prop = a[x].getAttribute('request_prop');
-
-                console.log('request_prop', request_prop);
-
+                
                 if(request_prop != null){
                     fieldsSearch[fieldsSearch.length] = { id: fieldCount, name: request_prop}
                 }
-            }
-
-            fieldCount++;
+            }            
+                                                
+            var idTooltip = a[x].getAttribute('id');            
             
-            btnDetail.style.top = (a[x].offsetTop + 6) + 'px';
-            btnDetail.style.left = (a[x].offsetLeft + 10) + 'px';                
-
-            btnDetail.setAttribute('data-tooltip-id', 'tol-' + a[x].getAttribute('id'))
-            btnDetail.style.position = 'fixed';
+            btnDetail.setAttribute('data-tooltip-id', 'tol-' + idTooltip)            
             btnDetail.style.background = 'green';
             btnDetail.style.color = '#fff';
             btnDetail.style.borderWidth = '1px';
@@ -144,42 +303,22 @@ function setBtnDetail(){
             btnDetail.style.fontSize = '10px';
             btnDetail.style.width = '16px';
             btnDetail.style.textAlign = 'center';
-            btnDetail.style.cursor = 'pointer';
-
-            document.body.appendChild(btnDetail);                
-
-            btnDetail.onclick = function(){
-
-                let toolTipId = this.getAttribute('data-tooltip-id');
-                let toolTip = document.getElementById(toolTipId);
-
-                if(this.style.backgroundColor == 'green'){
-                    //this.innerHTML = '-';                                                
-                    this.style.backgroundColor = 'red';
-                    toolTip.style.display = 'block';                         
-                } else {
-                    //this.innerHTML = '+';                                                
-                    toolTip.style.display = 'none'; 
-                    this.style.backgroundColor = 'green';
-                }                    
-            }
+            btnDetail.style.cursor = 'pointer';             
+            btnDetail.style.zIndex  = 99999999;
+            
+            $elementParent = $('#' + a[x].getAttribute('id')).parent();            
+            $elementParent.append('<div class="btnDetailP" data-tooltip-id="tol-' + idTooltip + '">' + fieldCount + '</div>');
+            fieldCount++;
         }        
-    }   
-    var btnBuscar = document.querySelectorAll('[value="Buscar"]');
-
-    if(btnBuscar != null && btnBuscar != undefined){
-
-    }
+    }       
 }
 
 function setStyleTooltip(field = null ){
 
     var a = [];
 
-    if(field != null){
+    if(field != null){        
         a[0] = document.getElementById(field.getAttribute('id'));
-
-        console.log('aaaa', a[0]);
     } else {
        a = document.getElementsByTagName('*');
     }
@@ -213,19 +352,23 @@ function setStyleTooltip(field = null ){
 
 function setInfoField(field){
     let info = '';
-    let templateInfo = `
+    let templateInfoRequest = `
         <span style='font-weight: bold; font-size: 12px;'>{0}:</span><br>
-        <span style='font-weight: bold; padding-left: 10px'>endpoint: </span><span><a href='{1}' target='_blank'>swagger</a></span><br>        
-        `
+        <span style='font-weight: bold; padding-left: 10px'>property: </span><span>{2}</span><br>        
+    `
 
-        // <span style='font-weight: bold; padding-left: 10px'>property: </span><span>{2}</span><br></br>
-        
-    // if(field.request != null && field.request != undefined){
-    //     info += templateInfo.replace('{0}', 'Request').replace('{1}', field.request.endpoint).replace('{2}', field.request.property)
-    // }
+    let templateInfoResponse = `
+        <span style='font-weight: bold; font-size: 12px;'>{0}:</span><br>
+        <span style='font-weight: bold; padding-left: 10px'>property: </span><span>{2}</span><br>
+        <span style='font-weight: bold; padding-left: 10px'>endpoint: </span><span><a href='{1}' target='_blank'>swagger</a></span><br>
+    `
+    
+    if(field.request != null && field.request != undefined){
+        info += templateInfoRequest.replace('{0}', 'Request').replace('{1}', field.request.endpoint).replace('{2}', field.request.property)
+    }
 
     if(field.response != null && field.response != undefined){
-        info += templateInfo.replace('{0}', 'Response').replace('{1}', field.response.endpoint).replace('{2}', field.response.property)
+        info += templateInfoResponse.replace('{0}', 'Response').replace('{1}', field.response.endpoint).replace('{2}', field.response.property)
     }
         
     return info;
@@ -241,8 +384,7 @@ function setInfoFieldsSearch(field){
 
     let fieldsPayload = '';    
 
-    fieldsSearch.forEach(function (item, indice, array) {
-        console.log(item, indice);
+    fieldsSearch.forEach(function (item, indice, array) {        
         fieldsPayload += '<span style="padding-left: 20px">' + item.id + ': ' + item.name + '</span><br>';        
     });    
 
@@ -250,8 +392,6 @@ function setInfoFieldsSearch(field){
         
     return info;
 }
-
-
 
 function showLoader(show = true){
     var divLoader = document.getElementById('divLoader');    
@@ -289,4 +429,49 @@ function getPageName(){
     let url = window.location.toString().split('portal/');
     let urlFinal = url[1].split('?')[0];
     return urlFinal;
+}
+
+function insertFormPage(){
+    var templateFormPage = `
+        <div id="divFormPopUp2" class="hidden-tooltip">
+            <form id="formTooltipField">                
+                <span class="label-ext">Campo:</span> <input type="text" class="form-readonly" id="dom_label" name="dom_label" readonly /><br />
+                <input type="hidden" id="idField" name="idField" />
+                <input type="hidden" id="idTela" name="idTela" />
+                <h6>Estrangulamento Status</h6>
+                <select id="status" name="status">
+                    <option value="nao-mapeado">Não Mapeado</option>
+                    <option value="mockado">Mockado</option>
+                    <option value="dev-done">Dev Done</option>
+                    <option value="estrangulamento-homologado">Homologado</option>
+                    <option value="estrangulamento-em-producao">Em produção</option>
+                </select>
+
+                <h6>DOM</h6>                
+                <span class="label-ext">id:</span> <input type="text" class="form-readonly" id="dom_id" name="dom_id" readonly/><br />                
+                <span class="label-ext">name:</span> <input type="text" class="form-readonly" id="dom_name" name="dom_name" readonly /><br />
+                <span class="label-ext">title:</span> <input type="text" class="form-readonly" id="dom_title" name="dom_title" readonly /><br />
+                <span class="label-ext">class:</span> <input type="text" class="form-readonly" id="dom_class" name="dom_class" readonly />
+            
+                <h6>Redmine</h6>
+                <span class="label-ext">ID Issue:</span> <input id="redmine_issue_id" name="redmine_issue_id" type="text" value="" />
+                <a id="a-link-redmine" href="#">ver no Redmine</a>
+                <h6>Swagger (Api V3)</h6>            
+                <span class="label-ext">Request:</span>
+                <ul>
+                    <li>prop_name: <input id="swagger_prop_req_name" name="swagger_prop_req_name" type="text" value="" /></li>
+                </ul>
+                <span class="label-ext">Response:</span>
+                <ul>
+                    <li>prop_name: <input id="swagger_prop_resp_name" name="swagger_prop_resp_name" type="text" value="" /></li>
+                    <li>Link Swagger: <input id="input-link_resp_swagger" name="input-link_resp_swagger" type="text" value="" /></li>
+                    <li>Swagger: <a id="a-link_resp_swagger" href="#">ver aqui</a></li>
+                </ul>
+                <hr />
+                <input type="submit" value="Salvar" />
+            </form>
+        </div>
+    `;    
+
+    $('body').append(templateFormPage);
 }
